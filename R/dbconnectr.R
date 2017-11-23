40a512d868a58e7db11acc13a73ebc732ce29115
Filip Schouwenaars <filip@datacamp.com>
2017-11-23 12:59:59 +0100

    add documentation, update travis

--- R/dbconnectr.R
+++ R/dbconnectr.R
@@ -6,12 +6,17 @@ get_parameter <- function(name) {
   fromJSON(paste(resp, collapse = "\n"))$Parameter$Value
 }
 
+#' Get a list of all databases.
+#'
 #' @export
 get_databases <- function() {
   dbstring <- get_parameter("/dbconnect/dbnames")
   strsplit(dbstring, split = ",")[[1]]
 }
 
+#' Create a DBI connection to a database.
+#'
+#' @param dbname character string specifying the database you want to connect to. Use \code{\link{get_databases}} to get a list of available databases.
 #' @export
 #' @importFrom DBI dbConnect
 create_connection <- function(dbname = "main-app") {
@@ -25,7 +30,13 @@ create_connection <- function(dbname = "main-app") {
   do.call(DBI::dbConnect, creds)
 }
 
+#' Open up the documentation of a database
+#'
+#' @param dbname character string specifying the database you want to connect to. Use \code{\link{get_databases}} to get a list of available databases.
+#' @param open boolean denoting whether or not to open the URL.
+#' @return The database documentation URL (invisibly).
 #' @export
+#' @importFrom utils browseURL
 get_docs <- function(dbname = "main-app", open = TRUE) {
   url <- get_parameter(sprintf("/dbconnect/%s/docs", dbname))
   message("Opening database documentation...")
